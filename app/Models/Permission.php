42b91aa8a28816c45f2ce6366840d60aafd65e98
Sohidul islam <113580855+sohidul8305@users.noreply.github.com>
2023-05-29 21:13:31 +0600

    First Commit with authentication

---
+++ app/Models/Permission.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Laratrust\Models\LaratrustPermission as PermissionModel;
+
+class Permission extends PermissionModel
+{
+    public $guarded = [];
+}
---
+++ app/Models/Role.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Laratrust\Models\LaratrustRole as RoleModel;
+
+class Role extends RoleModel
+{
+    public $guarded = [];
+}
