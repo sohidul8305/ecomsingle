188d4f7b724db9aab38197b2f32641836e5968da
Sohidul islam <113580855+sohidul8305@users.noreply.github.com>
2023-06-04 18:14:46 +0600

    All Category Page Setup With Table

--- resources/views/admin/allsubcategory.blade.php
+++ resources/views/admin/allsubcategory.blade.php
@@ -3,7 +3,39 @@
 All Sub Category - single Ecom
 @endsection
 @section('content')
-All Sub Category
+<div class="container-xxl flex-grow-1 container-p-y">
+    <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">page/</span>All Sub Category</h4>
+                  <!-- Bootstrap Table with Header - Light -->
+                  <div class="card">
+                    <h5 class="card-header">Available Sub category Information</h5>
+                    <div class="table-responsive text-nowrap">
+                      <table class="table">
+                        <thead class="table-light">
+                          <tr>
+                            <th>Id</th>
+                            <th>Sub Category Name</th>
+                            <th>Slug</th>
+                            <th>Product</th>
+                            <th>Actions</th>
+                          </tr>
+                        </thead>
+                        <tbody class="table-border-bottom-0">
+                          <tr>
+                                <td>1</td>
+                                <td>Electronics</td>
+                                <td>10</td>
+                                <td>100</td>
+                                <td>
+                                    <a href="" class="btn btn-primary">Edit</a>
+                                    <a href="" class="btn btn-warning">Delete</a>
+                                </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                  <!-- Bootstrap Table with Header - Light -->
+</div>
 @endsection
 
 
