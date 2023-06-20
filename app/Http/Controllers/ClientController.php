e653198c61b923ffc0b0b0d0d29fdbfeeab912e6
Sohidul islam <113580855+sohidul8305@users.noreply.github.com>
2023-06-20 23:42:43 +0600

    Displaying Single Product Information and Related Product Based on Sub Category From Database

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -14,8 +14,11 @@ class ClientController extends Controller
         $category = Category::findOrFail($id);
         return view('user_template.category', compact('category', 'products'));
     }
-    public function SingleProduct(){
-        return view('user_template.product');
+    public function SingleProduct($id){
+        $product = Product::findOrFail($id);
+        $subcat_id= Product::where('id', $id)->value('product_subcategory_id');
+        $related_products = Product::where('product_subcategory_id', $subcat_id)->latest()->get();
+        return view('user_template.product', compact('product', 'related_products'));
     }
     public function AddToCart(){
         return view('user_template.addtocart');
--- resources/views/user_template/product.blade.php
+++ resources/views/user_template/product.blade.php
@@ -4,21 +4,21 @@
     <div class="row">
        <div class="col-lg-4">
      <div class="box_main">
-        <div class="tshirt_img"><img src="images/dress-shirt-img.png"></div>
+        <div class="tshirt_img"><img src="{{ asset($product->product_img) }}"></div>
      </div>
         </div> 
         <div class="col-lg-8">
    <div class="box_main">
     <div class="product-info">
-        <h4 class="shirt_text text-left">Man-Shirt</h4>  
-    <p class="price_text text-left">Price  <span style="color: #262626;">$ 30</span></p>
+        <h4 class="shirt_text text-left">{{ $product->product_name }}</h4>  
+    <p class="price_text text-left">Price  <span style="color: #262626;">$ {{ $product->price }}</span></p>
     </div>
     <div class="my-3 product-details">
-        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident odio saepe optio. Veniam maxime ex, corporis id provident dolorem amet, facere magnam perspiciatis nemo ratione optio non eaque aperiam assumenda.</p>
+        <p class="lead">{{ $product->product_long_des }}</p>
    <ul class="p-2 bg-light my-2">
-    <li>Category- Electroics</li>
-    <li>Sub Category- Mobile</li>
-    <li>Availlable Quantity- 10</li>
+    <li>Category- {{ $product->product_category_name }}</li>
+    <li>Sub Category-{{ $product->product_subcategory_name }}</li>
+    <li>Availlable Quantity- {{ $product->quantity }}</li>
    </ul>
     </div>
 
@@ -37,18 +37,19 @@
                    <div class="fashion_section_2">
                          <div class="row">
                           
-                            <div class="col-lg-4 col-sm-4">
-                               <div class="box_main">
-                                     <h4 class="shirt_text">Product Name</h4>
-                                     <p class="price_text">Price <span style="color: #262626;">$ 50</span></p>
-                                     <div class="tshirt_img"><img src=""></div>
-                                     <div class="btn_main">
-                                        <div class="buy_bt"><a href="#">Buy Now</a></div>
-                                        <div class="seemore_bt"><a href="">See More</a></div>
-                                     </div>
-                               </div>
-                            </div>
-                         </div>
+                           @foreach ($related_products as $product)
+                           <div class="col-lg-4 col-sm-4">
+                              <div class="box_main">
+                                    <h4 class="shirt_text">{{ $product->product_name }}</h4>
+                                    <p class="price_text">Price <span style="color: #262626;">$ {{ $product->price }}</span></p>
+                                    <div class="tshirt_img"><img src="{{ asset($product->product_img) }}"></div>
+                                    <div class="btn_main">
+                                       <div class="buy_bt"><a href="#">Buy Now</a></div>
+                                       <div class="seemore_bt"><a href="{{ route('singleproduct', [$product->id, $product->slug]) }}">See More</a></div>
+                                    </div>
+                              </div>
+                           </div>
+                           @endforeach
                    </div>
                 </div>
     </div>
